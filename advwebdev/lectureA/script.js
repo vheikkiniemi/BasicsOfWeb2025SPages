625bdbad4c9f727addcf203b6b86a095a2a15d5a
Ville Heikkiniemi <ville.heikkiniemi@centria.fi>
2025-12-29 13:44:21 +0200

    quick fix

--- advwebdev/lectureA/script.js
+++ advwebdev/lectureA/script.js
@@ -5,6 +5,11 @@ document.getElementById('contactForm').addEventListener('submit', function (e) {
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
 
+    console.log("Form submitted:");
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Message:", message);
+
     alert(
         "Form submitted!\n\n" +
         "Name: " + name + "\n" +
